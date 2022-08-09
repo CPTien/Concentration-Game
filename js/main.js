903e72f9f3f5e4b109f2099d54d895d299a66c2e
patricktien <patrick1996112@gmail.com>
2022-08-08 17:05:50 -0700

    add img and some JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
-// focus on JS first before moving on to CSS/HTML
-
+const cards = document.querySelectorAll('.cell')
 const replayBtn = document.getElementById("replayBtn")
+const timer = document.getElementsByClassName("timer")
+console.log(cards)
 console.log(replayBtn)
+console.log(timer)
+
+
+
+//initialize board
+  // assign all the cards at random positions 
+function shuffleCards {
+ console.log("123")
+};
+
+
+
+//timer functions
+function countTime {
+
+}
+
+
+
+//game logic 
+  //see if first card = second card 
+    // if the same, faceup
+
+    //if not, facedown
+
+//restart functions
+  // initialize board
+
+ 
